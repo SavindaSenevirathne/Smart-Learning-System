3a2ffb5884cf81cf7eb906d4e0bdc203277b47d4
SavindaSenevirathne <savindadilsara@gmail.com>
2019-01-24 00:06:52 +0530

    test fail correction

--- client/src/app/my-courses/my-courses.component.spec.ts
+++ client/src/app/my-courses/my-courses.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MyCoursesComponent } from './my-courses.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('MyCoursesComponent', () => {
   let component: MyCoursesComponent;
@@ -8,7 +11,7 @@ describe('MyCoursesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MyCoursesComponent ]
+      declarations: [ MyCoursesComponent ], imports: [RouterTestingModule, FormsModule, HttpClientModule]
     })
     .compileComponents();
   }));
